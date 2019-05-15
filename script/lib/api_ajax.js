f3c7a6cb522f260cee0dc6ad2667095284f213ad
jindingtimes@jindingtimes.com <jindingtimes@jindingtimes.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2019-05-15 02:08:27 +0000

    正式环境，首页的banner的修改。
    git-svn-id: svn://svn7.apicloud.com/A6097629080312/callCenter@139 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/lib/api_ajax.js
+++ script/lib/api_ajax.js
@@ -2,8 +2,8 @@ var __CONFIG__ = {
     // baseUrl:'https://192.168.0.157:9000/', // 本地
     // baseUrl:'https://192.168.0.157:8081/', // 本地
     // baseUrl:'https://192.168.0.222:9000/', // 本地
-    baseUrl: 'https://calltest.jindinghaiju.com:9000/',  // 测试
-    // baseUrl: 'https://call.jindinghaiju.com/', // 正式
+    // baseUrl: 'https://calltest.jindinghaiju.com:9000/',  // 测试
+    baseUrl: 'https://call.jindinghaiju.com/', // 正式
     fixstr: 'dhi5ht798eh87dy9JLIdasfdHKHYUyjA'
 }
 // Object.assign pollify
